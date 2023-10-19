7ef53b7390f5004ace6eca105db823d613b539a1
kitamura-ryusei <141385505+kitamura-ryusei@users.noreply.github.com>
2023-10-19 21:43:55 +0000

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :customer, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/registrations"
+  }
+  
+  scope module: :public do
+  root to: "homes#top"
+  end
+  
+  namespace :admin do
+    root to: "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
